196fb6db82a8f4cbbaa1a806d2256599efe2f139
Selina Heller <feronia_aequitas@protonmail.com>
2022-04-27 22:19:19 +0200

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,6 +1,19 @@
-def add_time(start, duration):
+def add_time(start, duration, weekday = False):
+  
+  day = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
+  day_time = 24
+  
+  time, aMorPM = start.split(" ")
+  hours, mins = time.split(":")
+  durationHours, durationMins = duration.split(":")
 
+  #get the total hours and minutes of the new time
+  totalHours = int(hours) + int(durationHours)
+  totalMins = int(mins) + int(durationMins)
+
+  #clean the new hours and minutes up so it makes more sense
+  
 
 
 
